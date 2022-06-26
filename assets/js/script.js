258fc9fd7ebca5f620c36290534cc4b74de2f19a
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-06-26 10:01:50 -0400

    setTimeout endQuiz delay

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,30 @@
+
+var startBtn = document.querySelector("#start-btn");
+var pageContentEl =  document.querySelector("#main-content");
+var pageTitleEl = document.querySelector("#title");
+
+
+var endQuiz = function() {
+    pageTitleEl.textContent = "All Done!"
+    pageContentEl.textContent = ("Your Final Score is " + finalScore)
+};
+
+
+
+
+// timer value when last button is clicked [help]
+// var timeRemaining = startTimer.value;
+// var finalScore = timeRemaining;
+
+// var startQuiz = start timer & first quiz question
+function startQuiz() {
+console.log("time's up!");
+// test timer
+var startTimer = setTimeout(endQuiz, 75000);
+
+}
+
+
+
 // connect start button to timer
-var startBtn = document.querySelector("#start-btn")
-startBtn.addEventListener()
+startBtn.addEventListener("click", startQuiz);
