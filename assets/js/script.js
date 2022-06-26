aea8bc321fea2d672388af447d7b6357beed4532
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-06-26 14:06:25 -0400

    added first question on timer start

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,16 +5,24 @@ var pageTitleEl = document.querySelector("#title");
 var counterEl = document.querySelector("#timer");
 var viewScoreEl = document.querySelector("#score-page");
 console.dir(startBtn)
+
+  // Test Timer
+  var counter = 75;
+
+
 // End of Quiz Page
 var endQuiz = function() {
-    pageTitleEl.textContent = "All Done!"
-    pageContentEl.textContent = ("Your Final Score is ") 
+    pageTitleEl.textContent = "All Done!";
+    pageTitleEl.className = "end-title";
+    pageContentEl.textContent = ("Your Final Score is ");
     
     //  + finalScore)
 };
+// timer value when last button is clicked [help]
+// var timeRemaining = startTimer.value;
+// Test Score
+// var finalScore = count;
 
-  // Test Timer
-  var counter = 5;
 
 // Test Timer Countdown
 var count = function() {
@@ -35,19 +43,48 @@ function countdownEl() {
     }
 
 
-// timer value when last button is clicked [help]
-// var timeRemaining = startTimer.value;
-// Test Score
-// var finalScore = count;
+
 
 // var startQuiz = start timer & first quiz question
 function startQuiz() {
     console.log("start timer");
-    // // test timer starts
-    // var startTimer = setTimeout(endQuiz, 76000);
-    // call timer countdown
+    // Start timer countdown
     countdownEl();
 
+// display first question
+    // Question text
+    pageTitleEl.textContent = "Commonly used data types do NOT include: ";
+    // Question formatting
+    pageTitleEl.className = "questions";
+
+// display first question answer choices
+    // empty pageContentEl
+    pageContentEl.textContent = "";
+    pageContentEl.className = "answers-box"
+    // create ordered answer choice list
+    var answerChoices = document.createElement("ol");
+    pageContentEl.appendChild(answerChoices);
+    answerChoices.className = "answers";
+        // create list items
+        var answer1 = document.createElement("li");
+            answer1.className = "quiz-btn";
+            answer1.textContent = "1. strings";
+            answerChoices.appendChild(answer1);
+        var answer2 = document.createElement("li");
+            answer2.className = "quiz-btn";
+            answer2.textContent = "2. booleans";
+            answerChoices.appendChild(answer2);
+        var answer3 = document.createElement("li");
+            answer3.className = "quiz-btn";
+            answer3.textContent = "3. alerts";
+            answerChoices.appendChild(answer3);
+        var answer4 = document.createElement("li");
+            answer4.className = "quiz-btn";
+            answer4.textContent = "4. numbers";
+            answerChoices.appendChild(answer4);
+        
+    // remove start button
+    startBtn.remove();
 }
 
 
